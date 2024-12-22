001f18bf5dbc9fdf2ad5054cb54c7328c6854ddf
Nikita <nikita.k.0223@inspiria.edu.in>
2024-12-22 20:30:34 +0530

    Create Day-06.py

---
+++ 20 days challng/Day-06.py
@@ -0,0 +1,36 @@
+#TUPLE CONCATENTION
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5)
+combined = tuple1 + tuple2
+print(combined)  # Output: (1, 2, 3, 4, 5)
+
+
+#TUPLE INDEX
+my_tuple = (10, 20, 30)
+print(my_tuple[1])  # Output: 20
+
+#TUPLE LENGTH
+my_tuple = (10, 20, 30)
+print(len(my_tuple))  # Output: 3
+
+
+#TUPLE MEMBERSHIP
+my_tuple = (10, 20, 30)
+print(len(my_tuple))  # Output: 3
+
+
+#TUPLE REPETITION
+my_tuple = (1, 2)
+repeated = my_tuple * 3
+print(repeated) 
+# Output: (1, 2, 1, 2, 1, 2)
+
+
+#TUPLE SLICE
+my_tuple = (10, 20, 30, 40, 50)
+print(my_tuple[1:4])  # Output: (20, 30, 40)
+
+
+
+
+
